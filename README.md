fd598383f6c00e426f2373c48c1a53333d55e3bd
M.Haris Baig <harisbaig100@gmail.com>
2018-12-18 19:02:00 +0500

    MERN_STACK setup with authentication.
    MERN_STACK setup with authentication, navBar, footer signup, login logout and landing page.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# AVM-Group
+# aloha-CRM
+
+This is Customer-relationship management app, built on the top of MERN_STACK.
+
+## Available Scripts
+
+In the project directory, first run 
+
+### `npm install`
+
+then run this for the client's dependencies to install. 
+
+### `npm run client-install`
+
+To install dependencies.
+
+### change your mongodb credentials in config/js file
+
+Then to run app:
+
+### `npm run dev`
+
+which Runs the server and the client concurrently in development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view the client side of it in the browser.
+and [http://localhost:5000](http://localhost:5000) is the server running concurrently.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authAction';
+import { clearCurrentProfile } from './actions/profileAction';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import PrivateRoute from './components/privateRoute';
+
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Landing from './components/Landing';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import NotFound from './components/notFound';
+
+import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              
+              <Route exact path="/not-found" component={NotFound} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
